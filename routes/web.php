c91fab8f739c53cd988c2bebc067ad53aba74bf6
Noel Santiago Mendez Jaimes <nmendez643@unab.edu.co>
2026-02-17 14:29:20 -0500

    Grupo de rutas

--- routes/web.php
+++ routes/web.php
@@ -5,8 +5,10 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', HomeController::class);
 
-Route::get('/product', [ProductController::class, "index"]);
+Route::prefix("/product")->controller(ProductController::class)->group(function(){
+    Route::get('/', "index");
+    Route::get('/create', "create");
+    Route::get('/{id}/{categoria?}', "show");
+});
 
-Route::get('/product/create', [ProductController::class, "create"]);
 
-Route::get('/product/{id}/{categoria?}', [ProductController::class, "show"]);
