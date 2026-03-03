1829e3e022d45580c802dfc983383d6fa4e434b6
Noel Santiago Mendez Jaimes <nmendez643@unab.edu.co>
2026-03-03 13:53:44 -0500

    ORM Product: Se listo lo basico

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,12 +2,21 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Product;
 use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
     public function index(){
-        return view("product.index");
+        $productuctlist=Product::all();
+
+        //$product1 = $productuctlist[0];
+        //dd($productuctlist[0]);
+        //dd($product1);
+
+        return view("product.index", [
+            "milista"=>$productuctlist
+        ]);
     }
     public function create(){
         return view("product.create");
