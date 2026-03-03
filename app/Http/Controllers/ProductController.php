0c1347eaee2eab86cfa95dd908d9c18782c63f82
Noel Santiago Mendez Jaimes <nmendez643@unab.edu.co>
2026-03-03 15:06:12 -0500

    ORM Product: Fornulario basico

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Category;
 use App\Models\Product;
 use Illuminate\Http\Request;
 
@@ -19,8 +20,26 @@ class ProductController extends Controller
         ]);
     }
     public function create(){
-        return view("product.create");
+        $category = Category::all();
+        return view("product.create", [
+            "myCategorias" => $category
+        ]);
     }
+
+    public function store(Request $request){
+        $newProduct = new Product();
+        $newProduct->name = $request->input("nombre");
+        $newProduct->description = $request->input("description"); 
+        $newProduct->price = $request->input("precio");
+        $newProduct->category_id = $request->input("category_id");
+
+        $newProduct->save();
+        
+        // Redireccionamos a la ruta con nombre 'produc.index' (/product)
+        return redirect()->route('produc.index');
+    }
+
+
     public function show($id){
         return view("product.show");
 
--- routes/web.php
+++ routes/web.php
@@ -6,8 +6,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', HomeController::class);
 
 Route::prefix("/product")->controller(ProductController::class)->group(function(){
-    Route::get('/', "index");
+    Route::get('/', "index")->name("produc.index");
     Route::get('/create', "create");
+    Route::post('/store', "store")->name("produc.store");
     Route::get('/{id}', "show");
 });
 
